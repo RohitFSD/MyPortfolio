864cc98d00365602507e78c20465ee51e2f603ac
Rohit Nigam <rohitbet22@gmail.com>
2025-06-02 07:19:53 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -2,8 +2,13 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      {/* create a launch page */}
+      <header>
+        <h1>My Portfolio</h1>
+      </header>
     </div>
   );
 }
 
 export default App;
+// This is a simple React component that serves as the main entry point for the application.
